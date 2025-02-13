d90a1e6416c6b096cb95c1fa0de9ce0677875e3b
Danil <danilmfbushuev@gmail.com>
2025-02-13 14:51:50 +0100

    Added Devil Sigil

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,55 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-#include <conio.h>
+
+GLfloat point[] = {
+    //middle triangle
+    -1.0f, -0.5f, 0.0f,
+     1.0f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f,
+
+     //left triangle
+     1.0f,  1.0f, 0.0f,
+     0.0f, -1.0f, 0.0f,
+     0.0f,  0.5f, 0.0f,
+
+     //right triangle
+    -1.0f,  1.0f, 0.0f,
+     0.0f, -1.0f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
+GLfloat colors[] = {
+     1.0f,  0.0f,  0.0f,
+     1.0f,  0.0f,  0.0f,
+     1.0f,  0.0f,  0.0f,
+
+     1.0f,  0.0f,  0.0f,
+     1.0f,  0.0f,  0.0f,
+     1.0f,  0.0f,  0.0f,
+
+     1.0f,  0.0f,  0.0f,
+     1.0f,  0.0f,  0.0f,
+     1.0f,  0.0f,  0.0f
+};
+
+const char* vertex_shader =
+"#version 330\n"
+"layout (location = 0) in vec3 vertex_position;"
+"layout (location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main() {"
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_position, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 330\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main() {"
+"   frag_color = vec4(color, 1.0);"
+"}";
 
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
@@ -58,7 +106,45 @@ int main(void)
 
     std::cout << "Press 'Esc' to exit the program.\n" << std::endl;
 	
-	glClearColor(1, 1, 0, 1);
+	glClearColor(0, 0, 0, 0);
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vs);
+    glAttachShader(shader_program, fs);
+    glLinkProgram(shader_program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    GLuint points_vbo = 0;
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -66,6 +152,10 @@ int main(void)
 
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 9);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
